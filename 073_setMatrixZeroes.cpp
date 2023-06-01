c239b43bbeb7e4826f8058a3b00b9a4db74cd5d6
Rohan Singh <rohansinghrajput.dev@gmail.com>
2023-06-01 18:01:20 +0530

    add optimised O(1) method to setMetrixZero

--- 073_setMatrixZeroes.cpp
+++ 073_setMatrixZeroes.cpp
@@ -27,80 +27,131 @@ void printArray(vector<vector<int>> v){
 }
 
 
-// Brute force approach | O(n^2) time | O(m*n) space
+// // Brute force0 | O(n^2) time | O(m*n) space
 // void setZeroes(vector<vector<int>>& matrix) {
 //     vector<pair<int, int>> pos;  // {{1, 1}} or {{0, 0}, {0, 3}}
-//     for(int i=0; i<matrix.size(); i++){
-//         for(int j=0; j<matrix[i].size(); j++)
+//     int r = matrix.size(), c = matrix[0].size();
+//     for(int i=0; i<r; i++){
+//         for(int j=0; j<c; j++)
 //             if(matrix[i][j] == 0) pos.push_back(make_pair(i, j));
 //     }
 //     // for(auto& e: pos) cout << e.first << ", " << e.second << endl;
 //     for(auto& p: pos){
-//         for(int i=0; i<matrix[p.first].size(); i++){
-//             matrix[p.first][i] = 0;
-//         }
-//         for(int j=0; j<matrix.size(); j++){
-//             matrix[j][p.second] = 0;
-//         }
+//         for(int j=0; j<r; j++)
+//             matrix[j][p.second] = 0;  // set colums to zero
+//         for(int i=0; i<c; i++)
+//             matrix[p.first][i] = 0;  // set rows to zero
 //     }
 // }
 
-// // Brute force2: mark future 0 as -1 | (n*m)*(n+m)+(n*m) ~ O(n^3) time | O(1) space
+
+
+// // Brute force1: mark future 0 as -1 | (n*m)*(n+m)+(n*m) ~ O(n^3) time | O(1) space
 // void setZeroes(vector<vector<int>>& matrix){
-//     int odd = 1111;  // or -1
-//     for(int i=0; i<matrix.size(); i++){
-//         int cols = matrix[i].size();
-//         for(int j=0; j<cols; j++)
+//     int mark = -101;  // or -1
+//     int r = matrix.size();
+//     int c = matrix[0].size();
+//     for(int i=0; i<r; i++){
+//         for(int j=0; j<c; j++)
 //             if(matrix[i][j] == 0){
-//                 // --- mark row -1 ---
-//                 for(int k=0; k<cols; k++)
+//                 // ----- mark row -----
+//                 for(int k=0; k<c; k++)
 //                     if(matrix[i][k] != 0)
-//                         matrix[i][k] = odd;
-//                 // ||||| mark columns -1 |||||
-//                 for(int k=0; k<matrix.size(); k++)
+//                         matrix[i][k] = mark;
+//                 // ||| mark columns |||
+//                 for(int k=0; k<r; k++)
 //                     if(matrix[k][j] != 0)
-//                         matrix[k][j] = odd;
+//                         matrix[k][j] = mark;
 //             }
 //     }
-//     // change -1 -> 0
-//     for(int i=0; i<matrix.size(); i++){
-//         int cols = matrix[i].size();
-//         for(int j=0; j<cols; j++)
-//             if(matrix[i][j] == odd) 
+//     // change mark -> 0
+//     for(int i=0; i<r; i++){
+//         for(int j=0; j<c; j++)
+//             if(matrix[i][j] == mark) 
 //                 matrix[i][j] = 0;
 //     }
 // }
 
 
-// Brute Force3: mark rows and columns separately
-// O(2*m*n) time | O(m+n) space
+
+// // Better Approach: mark rows and columns separately
+// // O(2*m*n) time | O(m+n) space
+// void setZeroes(vector<vector<int>>& matrix){
+//     int mark = -101;  // or -1
+//     int r = matrix.size();
+//     int c = matrix[0].size();
+//     vector<int> rows(r, 0), cols(c, 0);
+
+//     // mark rows & cols
+//     for(int i=0; i<r; i++){
+//         for(int j=0; j<c; j++){
+//             if(matrix[i][j] == 0){
+//                 rows[i] = mark;
+//                 cols[j] = mark;
+//             }
+//         }
+//     }
+
+//     // mark matrix
+//     for(int i=0; i<r; i++){
+//         for(int j=0; j<c; j++){
+//             if(rows[i] == mark || cols[j] == mark){
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
+// }
+
+
+// OptimisedAlgo: O(n^2) time | O(1) space
 void setZeroes(vector<vector<int>>& matrix){
-    int mark = 101;
     int r = matrix.size();
     int c = matrix[0].size();
-    vector<int> rows(r, 0), cols(c, 0);
-
-    // mark rows & cols
+    int mark = 0;
+    int col0 = 1;
+    // check metrix and mark in-place rows & cols
     for(int i=0; i<r; i++){
         for(int j=0; j<c; j++){
             if(matrix[i][j] == 0){
-                rows[i] = mark;
-                cols[j] = mark;
+                // mark the i-th row (inside in-place rows)
+                matrix[i][0] = mark;
+                // mark the j-th col (inside in-place cols)
+                if(j != 0)
+                    matrix[0][j] = mark;
+                else
+                    col0 = mark;
             }
         }
     }
 
-    // mark matrix
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
-            if(rows[i] == mark || cols[j] == mark){
-                matrix[i][j] = 0;
+    // iterate & mark the matrix using the hash array (leaving hash array untouched).
+    // In-place hash array determines the matrix state so it's left untouched.
+    for(int i=1; i<r; i++){
+        for(int j=1; j<c; j++){
+            if(matrix[i][j] != 0){  // non-zero value -> 0.
+                // check for hashes and mark the matrix as 0.
+                // As i & j starts from 1, we won't be touching the hashes.
+                if(matrix[0][j] == mark || matrix[i][0] == mark){
+                    matrix[i][j] = 0;
+                }
+
             }
         }
     }
+
+    // Marking the in-place hashes (rows & cols).
+    // In-place col: if matrix[0][0] == mark => whole first row = 0.
+    if(matrix[0][0] == mark)
+        for(int j=0; j<c; j++)
+            matrix[0][j] = 0;
+    // In-place row: if col0 == mark => whole first column = 0.
+    if(col0 == mark)
+        for(int i=0; i<r; i++)
+            matrix[i][0] = 0;
 }
 
  
+
 int main(){
     vector<vector<int>> v1 = {
         {1, 1, 1},
